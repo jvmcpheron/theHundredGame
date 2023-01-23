e1378e41fcbee7f4ed7163fdff8a865efb9cf1b6
Jane McPheron <jvmcpheron@dyn-10-140-246-187.d.umn.edu>
2023-01-23 13:43:35 -0600

    the game is made

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # theHundredGame
+
+## Assignment Desciption
+
+The 100 game is a simple variant of Nim.  You and your opponent start with a joint total of 0.  You take turns adding a number from 1-10 to the total until the total gets to 100.  The player taking the total to 100 wins the game.  You will design a program that plays one player as the computer choosing largely random numbers and have the user act as the other player.
+
+Some things to note:
+
+- Your computer version should generate random guesses unless the current total is 90-99.  Then - it should pick whatever number will let it win.
+- You may assume all of the inputs will be integers, but you should include checks to make sure 
+  the integers they enter are legal (e.g., you should check if they selected a legal choice, did they pick a number not between 1 and 10, will their number put the total over 100, etc.).
+- You should let the player decide who is going to go first.
+- At the end of each game, the program should ask if you want to play again.
--- theHundredGame.cpp
+++ theHundredGame.cpp
@@ -9,10 +9,92 @@ Jane McPheron
 #include <cstdlib>
 using namespace std;
 
+
+//functions
+
+
+void gamePlay() {
+
+    //declaring varables
+    string playAgain;
+    int totalNumber;
+    int random;
+    int playerChoice;
+    bool okNum;
+
+    totalNumber = 0;
+
+    while (totalNumber < 100) {
+
+
+        //conditional checking if computer can win
+        if (totalNumber < 90){
+            //generate random number
+	        srand((unsigned) time(NULL));
+	        random = 1 + rand()%10;
+	
+        } else{
+            //placeholder for computer win code
+            random = 100 - totalNumber;
+        }
+
+        cout<< "The computer chose: " << random <<endl;
+        totalNumber += random;
+        cout << "The current total is: " << totalNumber << endl;
+
+        //conditional checking if comp already won
+        if (totalNumber < 100){
+            okNum = false;
+            //player's turn
+            while (okNum != true){
+                cout << "Pick a number 1-10: ";
+                cin >> playerChoice;
+
+                //checks for valid entry
+                if (playerChoice > 0){
+                    if (playerChoice < 11){
+                        okNum = true;
+                    } else{
+                        okNum = false;
+                        cout << "Not a valid number!" << endl;
+                    }   
+                } else{
+                    okNum = false;
+                    cout << "Not a valid number!" << endl;
+                }
+            }
+            totalNumber += playerChoice;
+            cout << "The current total is: " << totalNumber << endl;
+
+            //checking if you win
+            if (totalNumber == 100){
+                cout << "You win!!!";
+            } else if (totalNumber > 100){
+                cout << "You win!!!";
+            }
+
+
+
+
+
+        } else {
+            cout << "You lost!" << endl;
+        }
+
+    }
+
+    //checking if user wants to play again
+    cout << "Would you like to play again? (y/n)";
+    cin >> playAgain;
+    if (playAgain == "y"){
+        gamePlay();
+    }
+
+}
+
 int main(){
 
-    //code to generate random number between 1 and 10
-    cout << (rand() % 10) + 1<<" ";  
+    gamePlay();
 
 }
 
